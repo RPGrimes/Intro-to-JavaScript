d139f640679e8620c61ceec61a6e085091c91a55
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-23 16:45:06 +0100

    candy.test.js test passing

---
+++ shopping_basket/candy.test.js
@@ -0,0 +1,13 @@
+const Candy = require("./candy.js")
+
+describe("candy", () => {
+  it('names the new candy', () => {
+    const candy = new Candy("Mars", 4.99);
+    expect(candy.getName()).toBe('Mars')
+  });
+
+  it('prices the new candy', () => {
+    const candy = new Candy('Mars', 4.99);
+    expect(candy.getPrice()).toBe(4.99)
+  }
+)});
